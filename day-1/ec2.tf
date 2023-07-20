a8827f45c21c8bf0e4c7ae4a173e74603141ef8d
lpampolha <lpampolhas@gmail.com>
2023-07-20 15:36:13 -0300

    day-1 / Adição de alias

--- day-1/ec2.tf
+++ day-1/ec2.tf
@@ -9,8 +9,30 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Ubuntu
 }
 
+data "aws_ami" "ubuntu-east-1" {
+  provider = aws.east-1
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Ubuntu
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+resource "aws_instance" "web-east-1" {
+  provider      = aws.east-1
+  ami           = data.aws_ami.ubuntu-east-1.id
   instance_type = "t2.micro"
 
   tags = {
